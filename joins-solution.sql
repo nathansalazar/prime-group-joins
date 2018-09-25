d962874d241bdb690a24a6fbc3a6d3688764722d
nathansalazar <38958242+nathansalazar@users.noreply.github.com>
2018-09-25 14:57:29 -0500

    answer stretch-mode questions

--- joins-solution.sql
+++ joins-solution.sql
@@ -45,3 +45,30 @@ FROM "products"
 JOIN "warehouse_product" ON "products"."id"="warehouse_product"."product_id"
 JOIN "warehouse" ON "warehouse"."id"="warehouse_product"."warehouse_id"
 WHERE "description"='diet pepsi' GROUP BY "description";
+
+
+--9. How much was the total cost for each order?
+SELECT "first_name", "last_name", "order_date", "quantity", "unit_price", "description", "quantity"*"unit_price" AS "Order_Cost"
+FROM "customers"
+JOIN "addresses" ON "customers"."id" = "addresses"."customer_id"
+JOIN "orders" ON "orders"."address_id"="addresses"."id"
+JOIN "line_items" ON "orders"."id"="line_items"."order_id"
+JOIN "products" ON "products"."id"="line_items"."product_id";
+
+--10. How much has each customer spent in total?
+SELECT "first_name", "last_name", SUM("quantity"*"unit_price") AS "Total_Spent"
+FROM "customers"
+JOIN "addresses" ON "customers"."id" = "addresses"."customer_id"
+JOIN "orders" ON "orders"."address_id"="addresses"."id"
+JOIN "line_items" ON "orders"."id"="line_items"."order_id"
+JOIN "products" ON "products"."id"="line_items"."product_id"
+GROUP BY "first_name", "last_name";
+
+--11. How much has each customer spent in total? Customers who have spent $0 should still show up in the table. It should say 0, not NULL (research coalesce).
+SELECT "first_name", "last_name", COALESCE(SUM("quantity"*"unit_price"),0) AS "Total_Spent"
+FROM "customers"
+JOIN "addresses" ON "customers"."id" = "addresses"."customer_id"
+LEFT OUTER JOIN "orders" ON "orders"."address_id"="addresses"."id"
+LEFT OUTER JOIN "line_items" ON "orders"."id"="line_items"."order_id"
+LEFT OUTER JOIN "products" ON "products"."id"="line_items"."product_id"
+GROUP BY "first_name", "last_name";
